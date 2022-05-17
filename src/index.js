86a277d1a6cbd43e5f50432686ec49665200ab4c
Boryana Dimitrova <b.dimitrova97@gmail.com>
2022-05-17 14:55:39 +0300

    Connect mongoose and add 3 more .hbs files

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const path = require('path');
+const initDB = require('./config/database')
 
 const routes = require('./routes');
 const config = require('./config/config.json')[process.env.NODE_ENV || 'development'];
 
 const app = express();
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 require('./config/handlebars')(app);
 
 app.use(express.static(path.resolve(__dirname, './public')));
 app.use(routes);
 
-app.listen(config.PORT, console.log.bind(console, `Application is running on http://localhost:${config.PORT}`));
+initDB(config.DB_CONNECTION_STRING)
+    .then(() => {
+        app.listen(config.PORT, console.log.bind(console, `Application is running on http://localhost:${config.PORT}`));
+    })
+    .catch((err) => {
+        console.log('Application init failed: ', err);
+    })
